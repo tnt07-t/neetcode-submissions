eb9481a541762c2261885a81d66607742472ecd8
Tran Tran <tnt54@cornell.edu>
2026-06-11 12:58:03 +0700

    Add: redundant-connection - submission-2

---
+++ Data Structures & Algorithms/redundant-connection/submission-2.py
@@ -0,0 +1,30 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        N = len(edges)
+        par = [i for i in range(N+1)] #node "islands" -> initially each is its own
+        rank = [1] * (N+1) #size of each island
+
+        def find(n):
+            if n != par[n]:
+                return find(par[n])
+            return par[n]
+        def union(n1,n2):
+            p1, p2 = find(n1), find(n2)
+
+            #same root -> cycle
+            if p1 == p2:
+                return False
+
+            #merge smaller island to bigger island -> keeps tree flat
+            if rank[p1] > rank[p2]:
+                par[p2] = p1
+                rank[p1] += rank[p2]
+            else:
+                par[p1] = p2
+                rank[p2] += rank[p1]
+            return True
+        for n1,n2 in edges:
+            if not union(n1,n2):
+                return [n1,n2]
+
+        
