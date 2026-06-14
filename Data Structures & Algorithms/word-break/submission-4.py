d20512ddf1f6beba67766993adb3b676b6d4c943
Tran Tran <tnt54@cornell.edu>
2026-06-14 13:08:12 +0700

    Add: word-break - submission-4

---
+++ Data Structures & Algorithms/word-break/submission-4.py
@@ -0,0 +1,21 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        cache = [None] * len(s) # None for unvisited
+        #cache that tracks if s can be split at index bla bla
+
+        def check(index):
+            if index == len(s):
+                return True
+            if cache[index] is not None:
+                return cache[index]
+
+            for word in wordDict:
+                if s.startswith(word,index):
+                    if check(index + len(word)):
+                        cache[index] = True
+                        return True
+                        
+            cache[index] = False
+            return False
+
+        return check(0)
