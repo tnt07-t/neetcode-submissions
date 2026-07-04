830639ad6008fefdfed1ac0b3936b6724c077882
Tran Tran <tnt54@cornell.edu>
2026-07-04 15:44:37 +0700

    Add: design-word-search-data-structure - submission-6

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-6.cpp
@@ -0,0 +1,46 @@
+class WordDictionary {
+    struct Node {
+        unordered_map<char,Node*> children;
+        bool isEnd = false;
+    };
+    Node* root;
+
+public:
+    WordDictionary() {
+        root = new Node();
+    }
+    
+    void addWord(string word) {
+        //curr is not a node, but pointer to a node; & gets address of root node
+        Node* curr = root; 
+
+        for (char c : word){
+            if (!curr->children.count(c)){
+                curr->children[c] = new Node(); //new node
+            }
+            curr = curr->children[c]; //move to child
+        }
+        curr->isEnd = true; //mark end
+    }
+    
+    bool search(string word) {
+        return dfs(root, 0, word);
+    }
+
+    private:
+    bool dfs(Node* node, int pos, string word){
+        if (pos == word.size())return node->isEnd;
+
+        char c = word[pos];
+
+        if (c == '.'){ //any ch works 
+            for (auto& [ch,next] : node->children)
+                if (dfs(next, pos + 1, word)) return true;
+            return false;
+        }
+        else{ // ch must match c
+            if (node->children.count(c) and dfs(node->children[c], pos+1,word)) return true;
+            return false;
+            }
+        }
+};
